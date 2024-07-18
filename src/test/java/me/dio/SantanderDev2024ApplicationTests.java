25eeb134572572c4ed81cef47f51e8d92b92f869
Debora Cristina <deboracristinasantos5@gmail.com>
2024-07-18 16:05:18 -0300

    API RESTful

---
+++ src/test/java/me/dio/SantanderDev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
